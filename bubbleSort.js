88fed2edf8ccd742acb6522edbc569ce8ef6b576
Jared Blumer <jaredgblumer@gmail.com>
2018-08-25 16:08:47 -0400

    Add Bubble Sort Algorithm

---
+++ bubbleSort.js
@@ -0,0 +1,21 @@
+// Selection Sort in Javascript
+// Author: Jared Blumer
+
+function bubbleSort(arr) {
+
+  for (var i = arr.length - 1; i > 0; i--) {
+
+    for (var j = 0; j <= i; j++) {
+
+      if (arr[j] > arr[j+1]) {
+
+        // Swap values
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}

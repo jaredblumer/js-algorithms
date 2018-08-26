62c943e8c8cd3463eb1b68befd89d2226b678ae7
Jared Blumer <jaredgblumer@gmail.com>
2018-08-25 21:50:08 -0400

    Update variable names for clarity

--- linearSearch.js
+++ linearSearch.js
@@ -1,12 +1,12 @@
 // Linear Search in Javascript
 // Author: Jared Blumer
 
-function linearSearch(key, arr) {
+function linearSearch(target, arr) {
   var length = arr.length;
 
   for (var i = 0; i < length; i++) {
-    if (arr[i] === key) return i;
+    if (arr[i] === target) return i;
   }
 
-  return null;
+  return -1;
 }

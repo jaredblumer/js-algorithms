e234fc1a4332172d55220073b91653e4664f2f0a
Jared Blumer <jaredgblumer@gmail.com>
2018-08-27 21:38:19 -0400

    Add Merge Sort Algorithm

---
+++ mergeSort.js
@@ -0,0 +1,36 @@
+// Merge Sort in Javascript
+// Author: Jared Blumer
+
+function mergeSort(arr) {
+
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  var middle = Math.floor(arr.length / 2),
+      left = arr.slice(0, middle),
+      right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+
+}
+
+function merge(left, right) {
+
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex++]);
+    } else {
+      result.push(right[rightIndex++]);
+    }
+
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+
+}
